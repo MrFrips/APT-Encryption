899c823d77658414452b28f7ef90dd930bcfd4dd
MrFrips <port.toshka@gmail.com>
2023-03-22 12:03:51 +0300

    add text ver: 0.2.5

--- Key.cs
+++ Key.cs
@@ -56,6 +56,7 @@ namespace encryption
             //Properties.Settings.Default.StringText();
             //Properties.Settings.Default.СolumnText();
             //Properties.Settings.Default.Save();
+            //Привет Мишаня!
             
         }
 
